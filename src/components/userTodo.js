d63960819601dc934fb72c4baaa6fbddedc0ac3c
Gzim <gzimasanni@gmail.com>
2021-07-07 13:53:23 +0200

    Add todos reducer and component

---
+++ src/components/userTodo.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import StatusFilter from './StatusFilter';
+import { fetchTodos } from '../actions';
+import { connect } from 'react-redux';
+import GetUsers from './getUsers';
+
+class UserTodo extends React.Component {
+  componentDidMount() {
+    this.props.fetchTodos()
+  }
+
+  returnTodos = () => {
+    return this.props.todos.map(todo => {
+      return (
+        <div className="card container d-flex" key={todo.id}>
+          <div className="card-body">
+            <div className="d-flex">
+              <h3>Title:</h3>
+              <h3 className="card-title text-danger">{todo.title}</h3>
+            </div>
+            <div className="card-text text-success">Completed: {todo.completed.toString()}</div>
+            Author: <GetUsers userId={todo.userId} />
+          </div>
+        </div>
+      )
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        < StatusFilter />
+        <div>
+          {this.returnTodos()}
+        </div>
+
+      </div>
+    );
+  };
+};
+
+const getMapToProps = (state) => {
+  console.log(state.todos)
+  return { todos: state.todos }
+}
+
+export default connect(getMapToProps, { fetchTodos })(UserTodo)
---
+++ src/reducers/todosReducer.js
@@ -0,0 +1,9 @@
+export default (state = [], action) => {
+  switch (action.type) {
+    case 'FETCH_TODOS':
+      return action.payload
+
+    default:
+      return state;
+  };
+};
