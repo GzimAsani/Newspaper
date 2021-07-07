dc6212d0bd0d0deb9ede822f541802cb1c753dba
Gzim <gzimasanni@gmail.com>
2021-07-07 20:07:20 +0200

    Add styling to the filter

--- src/components/statusFilter.js
+++ src/components/statusFilter.js
@@ -1,5 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { filterUsers } from '../actions';
+import style from '../assets/filter.css'
 
 const StatusFilter = () => {
   const filter = useSelector((state) => state.filter);
@@ -8,7 +9,7 @@ const StatusFilter = () => {
     dispatch(filterUsers(e.target.value));
   };
   return (
-    <select name="filter" id="filter" onChange={handleChange}>
+    <select name="filter" className={style} id="filter" onChange={handleChange}>
       {filter.possible.map((possibleFilter, index) => (
         <option value={possibleFilter} key={String(index)}>
           {possibleFilter}
