bd15fc97f949134e6f653d3bbe817edee5dd100a
Gzim <gzimasanni@gmail.com>
2021-07-07 14:18:25 +0200

    Add all my reducers inside the combine reducers

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,24 @@
+import React from 'react';
+import GetPosts from '../components/getPosts';
+import Navbar from '../components/Navbar';
+import UserShow from '../components/userShow';
+import Main from '../components/Main';
+import ShowPage from './showPage';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>gz</h1>
-      </header>
+    <div>
+      <BrowserRouter>
+        < Navbar />
+        <Switch>
+          <Route path="/" exact component={Main} />
+          <Route path="/posts" exact component={ShowPage} />
+          <Route path="/show/:id" exact component={UserShow} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
